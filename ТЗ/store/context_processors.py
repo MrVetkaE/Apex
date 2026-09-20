1a2a1e2fca123a7441ec5d0cbf1449cb8b4736f7
MrVetkaE <tostadacat32@gmail.com>
2026-09-20 14:05:43 +0500

    Delete ТЗ directory

--- ТЗ/store/context_processors.py
+++
@@ -1,9 +0,0 @@
-def cart_context(request):
-    """
-    Context processor providing cart summary from session.
-    """
-    cart = request.session.get('cart', {})
-    total_items = sum(cart.values()) if isinstance(cart, dict) else 0
-    return {
-        'cart_count': total_items,
-    }
